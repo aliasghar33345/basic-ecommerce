895789d5b68afd70e9f20fd6d83a4f29a876ce64
aliasghar33345 <aliasghar33345@gmail.com>
2021-06-15 17:08:37 +0500

    update 15-june

--- ecommerce/lib/home_page.dart
+++ ecommerce/lib/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:ecommerce/core/const.dart';
 import 'package:ecommerce/core/my_flutter_app.dart';
+import 'package:ecommerce/detail_page.dart';
 import 'package:ecommerce/models/shoes_models.dart';
+import 'package:ecommerce/widget/app_cliper.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class HomePage extends StatefulWidget {
  
@@ -39,7 +42,53 @@ class _HomePageState extends State<HomePage> {
             ],
           ),
         ),
-        Text("Display Slider"),
+        Container(
+          height: 300,
+          margin: EdgeInsets.symmetric(vertical:16),
+          child: ListView.builder(
+            itemCount: shoeList.length,
+            scrollDirection: Axis.horizontal,
+            physics: BouncingScrollPhysics(),
+            padding: EdgeInsets.symmetric(horizontal:16),
+            itemBuilder: (context, index){
+              return  GestureDetector(
+                onTap: (){
+                  Navigator.of(context).push(
+                   MaterialPageRoute(builder: (_) => DetailPage(
+                     shoeList[index],
+                   ),
+                   ),
+                  );
+                },
+                              child: Container(
+                  width: 230,
+                  margin: EdgeInsets.only(right:15),
+                  child: Stack(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 25),
+                        child: _buildbackground(index , 230),
+                      ),
+                      Positioned(
+                        bottom: 130,
+                        right: 10,
+                          child: Transform.rotate(
+                          angle: -math.pi / 7 ,
+                            
+                            child: Image(
+                            width: 220,
+                            image: AssetImage("assets/${shoeList[index].imgPath}"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+              
+          }),
+        ),
+       SizedBox(height: 16),
         Padding(padding: EdgeInsets.symmetric(horizontal: 16),child: 
         Row(
            mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -54,64 +103,74 @@ class _HomePageState extends State<HomePage> {
         ),
         SizedBox(height: 24),
        ...shoeList.map((data){
-          return Container(
-            margin: EdgeInsets.only(left:16 , right:16 , bottom:10),
-            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(
-                Radius.circular(25),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black12,
-                  blurRadius: 10,
-                  
+          return GestureDetector(
+             onTap: (){
+                  Navigator.of(context).push(
+                   MaterialPageRoute(builder: (_) => DetailPage(
+                     data,
+                   ),
+                   ),
+                  );
+                },
+                      child: Container(
+              margin: EdgeInsets.only(left:16 , right:16 , bottom:10),
+              padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(25),
                 ),
-              ],
-            ),
-            child: Row(children: <Widget> [
-              Image(
-                  image: AssetImage("assets/${data.imgPath}"),
-                  width: 100,
-                  height: 60,
-              ),
-              SizedBox(width: 16),
-              Expanded(
-                    child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                      Container(
-                        width: MediaQuery.of(context).size.width * 4,
-
-                        child: Text("${data.name}"
-                        ,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                   style: TextStyle(
-                     fontWeight: FontWeight.bold,
-                   ),
-                   ),
-                      ),
-                      Text("${data.brand}",
-                   style: TextStyle(
-                     color: Colors.black26,
-                     height: 1.6
-                   ),),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 10,
+                    
+                  ),
                 ],
+              ),
+              child: Row(children: <Widget> [
+                Image(
+                    image: AssetImage("assets/${data.imgPath}"),
+                    width: 100,
+                    height: 60,
                 ),
-              ) ,    
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                child: Text(
-                   "\$${data.price.toInt()}",
-                   style: TextStyle(
-                     fontWeight: FontWeight.bold,
-                     fontSize: 18
-                   ),
-                ),
-              ) ,
-            ],
+                SizedBox(width: 16),
+                Expanded(
+                      child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width * 4,
+
+                          child: Text("${data.name}"
+                          ,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                     style: TextStyle(
+                       fontWeight: FontWeight.bold,
+                     ),
+                     ),
+                        ),
+                        Text("${data.brand}",
+                     style: TextStyle(
+                       color: Colors.black26,
+                       height: 1.6
+                     ),),
+                  ],
+                  ),
+                ) ,    
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: Text(
+                     "\$${data.price.toInt()}",
+                     style: TextStyle(
+                       fontWeight: FontWeight.bold,
+                       fontSize: 18
+                     ),
+                  ),
+                ) ,
+              ],
+              ),
             ),
           );
       }),
@@ -156,5 +215,79 @@ class _HomePageState extends State<HomePage> {
 
 
     );
+  }
+
+  Widget _buildbackground(int index , double width)
+    
+ {
+    return ClipPath(
+        clipper: AppClipper(
+          cornerSize: 25 , diagonalHeight: 180,
+        ) ,
+                  child: Container(
+              color: shoeList[index].color,
+              width: width,
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Column(
+                        
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Icon(
+                              shoeList[index].brand == "Nike" ? MyFlutterApp.check : MyFlutterApp.stars
+                              ,
+                            size: 30,
+                            color: Colors.white,
+                            ),
+                          ),
+                          Expanded(child: SizedBox()),
+                           Container(
+                             width: 125,
+                             child: Text("${shoeList[index].name}",
+                             style: TextStyle(
+                               color:Colors.white,
+                               
+                             ),
+                             ),
+                           ),
+                           SizedBox(height:10),
+                           Text("${shoeList[index].price}",
+                           style: TextStyle(
+                             color:Colors.white,
+                             fontWeight: FontWeight.bold
+                           ),
+                           ),
+                           SizedBox(height: 16,),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                        child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: AppColors.greenColor ,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                          ),
+                        ),
+                        child: Center(
+                          child: Icon(
+                            MyFlutterApp.add,
+                            color: Colors.white,
+                          ),
+                          ),
+                      ),
+                    ),
+                  ],
+                ),
+        ),
+      );
   }
 }
